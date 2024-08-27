2b9120ef12fb7a365c284f964fa907dbf93462e4
BÙI ANH QUỐC <anhquocydkl@gmail.com>
2024-08-27 18:13:19 +0700

    Excercise_if_else

---
+++ WEEK1/Excercise_if_else.java
@@ -0,0 +1,15 @@
+public class Excercise_if_else {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        if ( x == y)
+        {
+            System.out.println("x = y");
+        }
+
+        else if ( x < y)
+        {
+            System.out.println("x < y");
+        }
+    }
+}
