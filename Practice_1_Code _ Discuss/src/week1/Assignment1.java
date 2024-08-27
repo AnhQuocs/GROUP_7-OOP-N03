f45fbc92408450f972359a77a8dd3d3501b9ba22
donttink <84282892+donttink@users.noreply.github.com>
2024-08-27 07:32:21 +0700

    Update Assignment1.java

--- Practice_1_Code _ Discuss/src/week1/Assignment1.java
+++ Practice_1_Code _ Discuss/src/week1/Assignment1.java
@@ -8,6 +8,6 @@ public class Assignment1 {
 		n2.i = 5;
 		n1.i = n2.i;
 		n2.i = 10;
-		System.out.println("n1.i = " + n1.i); // This will output the value of n1.i
+		System.out.println("n1.i = " + n1.i); 
 	}
 }
