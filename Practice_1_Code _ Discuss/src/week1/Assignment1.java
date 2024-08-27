55901ccf0b8f6d9153929b6f84b7fbb1bdac818f
donttink <tu0742005@gmail.com>
2024-08-27 07:30:15 +0700

    First commit
    Hard to try

---
+++ Practice_1_Code _ Discuss/src/week1/Assignment1.java
@@ -0,0 +1,13 @@
+package week1;
+
+public class Assignment1 {
+	public static void main(String[] args) {
+		Number n1 = new Number();
+		Number n2 = new Number();
+		n1.i = 2;
+		n2.i = 5;
+		n1.i = n2.i;
+		n2.i = 10;
+		System.out.println("n1.i = " + n1.i); // This will output the value of n1.i
+	}
+}
