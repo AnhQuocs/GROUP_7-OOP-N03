f9e5be99abe049196094668c970a40b782649b73
BÙI ANH QUỐC <anhquocydkl@gmail.com>
2024-09-02 17:09:35 +0700

    Initialization and Cleanup

---
+++ Main/Java/Com/Mycompany/App/dog.java
@@ -0,0 +1,12 @@
+public class dog 
+{
+    public void bark()
+    {
+        System.out.println("Gau");
+    }
+    
+    public void bark(String str)
+    {
+        System.out.println(str);
+    }
+}
