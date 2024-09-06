3f35fbd6f77bcf50fd83c7fb0366cf6a62f74cdb
BÙI ANH QUỐC <anhquocydkl@gmail.com>
2024-09-06 11:08:57 +0700

    ss

---
+++ Practice_2/src/main/java/com/mycompany/app/Access Control/AccessControl_2.java
@@ -0,0 +1,26 @@
+
+import java.io.PrintStream;
+
+public class AccessControl_2 
+{
+    public static void print(Object obj) 
+    {
+        System.out.println(obj);
+    }    
+
+    public static void print()
+    {
+        System.out.println();
+    }
+
+    public static void printnb(Object obj)
+    {
+        System.out.println(obj);
+    }
+
+    public static PrintStream
+    printf(String format, Object... args)
+    {
+        return System.out.printf(format, args);
+    }
+}
