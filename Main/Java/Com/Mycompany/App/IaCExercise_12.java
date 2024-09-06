830a2a3173abaa6975309259e8ec48937e9dbac3
BÙI ANH QUỐC <anhquocydkl@gmail.com>
2024-09-06 09:48:58 +0700

    Initialization and Cleanup

---
+++ Main/Java/Com/Mycompany/App/IaCExercise_12.java
@@ -0,0 +1,17 @@
+class Tank
+{
+    protected void finalize()
+    {
+        System.out.println("Hahaha");
+    }
+}
+
+public class IaCExercise_12 
+{
+    public static void main(String[] args) 
+    {
+        Tank obj = new Tank();
+        obj = null;
+        System.gc();
+    }
+}
