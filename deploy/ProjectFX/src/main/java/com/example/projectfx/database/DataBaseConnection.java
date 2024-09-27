302c8599ef53552ca0f0f80f3fa261882f6e103b
BÙI ANH QUỐC <anhquocydkl@gmail.com>
2024-09-27 09:56:12 +0700

    fix

--- deploy/ProjectFX/src/main/java/com/example/projectfx/database/DataBaseConnection.java
+++ deploy/ProjectFX/src/main/java/com/example/projectfx/database/DataBaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DataBaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/StudentDB?useSSL=false&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://localhost:3306/StudentDB?useSSL=false&allowPublicKeyRetrieval=true ";
     private static final String USER = "root";
     private static final String PASSWORD = "30072005"; // Đổi thành mật khẩu MySQL của bạn
 
